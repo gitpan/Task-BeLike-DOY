4059d403a74aa8606be77d5ba9a53c6d7fd5a00a
Jesse Luehrs <doy@cpan.org>
2010-05-02 19:23:56 +0000

    Import of DOY/Task-BeLike-DOY-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-DOY
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DOY/Task-BeLike-DOY-0.01.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/DOY.pm
@@ -0,0 +1,156 @@
+package Task::BeLike::DOY;
+BEGIN {
+  $Task::BeLike::DOY::VERSION = '0.01';
+}
+#ABSTRACT: modules I use
+
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::BeLike::DOY - modules I use
+
+=head1 VERSION
+
+version 0.01
+
+=head1 TASK CONTENTS
+
+=head2 Useful apps
+
+=head3 App::Ack
+
+=head3 App::Nopaste 0.20
+
+Version 0.20 required because: has Codepeek pastebin
+
+=head3 App::Termcast 0.06
+
+=head3 KiokuDB::Navigator
+
+=head2 Development tools
+
+=head3 Carp::Always::Color 0.03
+
+=head3 Carp::REPL
+
+=head3 Devel::Cover
+
+=head3 Devel::NYTProf 3.00
+
+=head3 Devel::REPL
+
+=head3 Dist::Zilla 2.00
+
+=head3 Module::Setup
+
+=head2 Useful optional deps for things
+
+=head3 Archive::Zip
+
+=head3 B::Keywords
+
+=head3 LWP
+
+=head3 Module::Refresh
+
+=head3 Pod::Coverage::TrustPod
+
+=head3 Scalar::Defer
+
+=head3 Term::ReadLine::Gnu
+
+=head3 Test::Pod
+
+=head3 Test::Pod::Coverage
+
+=head3 YAML
+
+=head2 CPAN interaction
+
+=head3 App::cpanminus 1.00
+
+=head3 CPAN::Mini
+
+=head3 CPAN::Mini::Webserver
+
+=head2 Modules I use a lot
+
+=head3 Moose 1.00
+
+=head3 Path::Class
+
+=head3 WWW::Mechanize
+
+=head2 Dependencies for my non-CPAN apps
+
+=head3 IO::Pty::Easy 0.08
+
+=head3 Net::GitHub 0.06
+
+Version 0.06 required because: includes ::V2
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-task-belike-doy at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-BeLike-DOY>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Task::BeLike::DOY
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-BeLike-DOY>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-BeLike-DOY>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-DOY>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-BeLike-DOY>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
