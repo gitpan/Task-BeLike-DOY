81750987a069873754cde37dfe51e90801049163
Jesse Luehrs <doy@cpan.org>
2010-06-13 20:53:12 +0000

    Import of DOY/Task-BeLike-DOY-0.02 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-DOY
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DOY/Task-BeLike-DOY-0.02.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/DOY.pm
+++ lib/Task/BeLike/DOY.pm
@@ -1,6 +1,6 @@
 package Task::BeLike::DOY;
 BEGIN {
-  $Task::BeLike::DOY::VERSION = '0.01';
+  $Task::BeLike::DOY::VERSION = '0.02';
 }
 #ABSTRACT: modules I use
 
@@ -16,83 +16,102 @@ Task::BeLike::DOY - modules I use
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 TASK CONTENTS
 
 =head2 Useful apps
 
-=head3 App::Ack
+=head3 L<App::Ack>
 
-=head3 App::Nopaste 0.20
+=head3 L<App::Nopaste> 0.20
 
 Version 0.20 required because: has Codepeek pastebin
 
-=head3 App::Termcast 0.06
+=head3 L<App::Termcast> 0.06
 
-=head3 KiokuDB::Navigator
+=head3 L<KiokuDB::Navigator>
 
 =head2 Development tools
 
-=head3 Carp::Always::Color 0.03
+=head3 L<Carp::Always::Color> 0.03
 
-=head3 Carp::REPL
+=head3 L<Carp::REPL>
 
-=head3 Devel::Cover
+=head3 L<Devel::Cover>
 
-=head3 Devel::NYTProf 3.00
+=head3 L<Devel::NYTProf> 3.00
 
-=head3 Devel::REPL
+=head3 L<Devel::REPL>
 
-=head3 Dist::Zilla 2.00
+=head3 L<Devel::Trace>
 
-=head3 Module::Setup
+=head3 L<Dist::Zilla> 4.00
+
+=head3 L<Dist::Zilla::PluginBundle::DOY>
+
+=head3 L<Module::Setup>
+
+=head3 L<Pod::Weaver::Section::Template>
+
+=head3 L<Task::SDK::Moose>
 
 =head2 Useful optional deps for things
 
-=head3 Archive::Zip
+=head3 L<Archive::Zip>
 
-=head3 B::Keywords
+=head3 L<B::Keywords>
 
-=head3 LWP
+=head3 L<LWP>
 
-=head3 Module::Refresh
+=head3 L<Module::Refresh>
 
-=head3 Pod::Coverage::TrustPod
+=head3 L<Pod::Coverage::TrustPod>
 
-=head3 Scalar::Defer
+=head3 L<Scalar::Defer>
 
-=head3 Term::ReadLine::Gnu
+=head3 L<Term::ReadLine::Gnu>
 
-=head3 Test::Pod
+=head3 L<Test::Pod>
 
-=head3 Test::Pod::Coverage
+=head3 L<Test::Pod::Coverage>
 
-=head3 YAML
+=head3 L<YAML>
 
 =head2 CPAN interaction
 
-=head3 App::cpanminus 1.00
+=head3 L<App::cpanminus> 1.00
 
-=head3 CPAN::Mini
+=head3 L<App::cpanoutdated> 0.05
 
-=head3 CPAN::Mini::Webserver
+Version 0.05 required because: for offline mode
+
+=head3 L<CPAN::Mini>
+
+=head3 L<CPAN::Mini::Webserver>
 
 =head2 Modules I use a lot
 
-=head3 Moose 1.00
+=head3 L<Moose> 1.00
 
-=head3 Path::Class
+=head3 L<Path::Class>
 
-=head3 WWW::Mechanize
+=head3 L<WWW::Mechanize>
 
 =head2 Dependencies for my non-CPAN apps
 
-=head3 IO::Pty::Easy 0.08
+=head3 L<IO::Pty::Easy> 0.08
 
-=head3 Net::GitHub 0.06
+=head3 L<Net::GitHub> 0.06
 
 Version 0.06 required because: includes ::V2
+
+=begin taskweaver_parsing_is_broken
+
+
+
+
+=end taskweaver_parsing_is_broken
 
 =head1 BUGS
 
@@ -129,17 +148,6 @@ L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-DOY>
 L<http://search.cpan.org/dist/Task-BeLike-DOY>
 
 =back
-
-=head1 AUTHOR
-
-  Jesse Luehrs <doy at tozt dot net>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2010 by Jesse Luehrs.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
 
 =head1 AUTHOR
 
